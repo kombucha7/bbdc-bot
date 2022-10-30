822cf45df98d43b4b8e1a9d3ca446cea6466628f
kombucha7 <29064557+kombucha7@users.noreply.github.com>
2022-10-30 13:24:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.wait import WebDriverWait
 
 from bot import send_message
 
