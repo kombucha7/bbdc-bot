b6184258b96272f31aad26c82d6987b64c4672eb
kombucha7 <29064557+kombucha7@users.noreply.github.com>
2022-12-11 13:44:46 +0800

    Updated to new site

--- main.py
+++ main.py
@@ -14,10 +14,17 @@ def job():
     except Exception as e:
         print(e)
 
+def checkTime():
+    current_hour = time.localtime().tm_hour
+    return current_hour < 23 and current_hour > 14
 
 if __name__ == "__main__":
     job()  # test
     schedule.every(interval).minutes.do(job)
     while True:
-        schedule.run_pending()
-        time.sleep(1)
+        if checkTime():
+            schedule.run_pending()
+            time.sleep(1)
+        else:
+            print("night sleep")
+            time.sleep(3600)
